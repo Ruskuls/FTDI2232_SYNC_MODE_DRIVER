d9dac5f9a550476e54c313d0be701c1eb00295ce
Rinalds <rinalds.ruskuls@gmail.com>
2017-09-07 05:35:07 +0000

    Inserted function from FTDI_MID_LEVEL.c, tested with bmp and jpg images. Problem seems to be that new function has small problems with at least byte. After transmitting and receiving jpg image it's corrupted

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include "../ftd2xx.h"
 #include "FTDI_LOW_LEVEL.h"
-//#include "FTDI_MID_LEVEL.h"
+#include "FTDI_MID_LEVEL.h"
 
 FT_HANDLE ftHandle;
 FT_STATUS ftStatus;
@@ -20,10 +20,12 @@ int main(int argc, char *argv[])
 	bool FT_ShowDebugInformation = true;
 	UCHAR Mask = 0xFF;
 	UCHAR BitMode = 0;
-	UCHAR LatencyTimer = 5;
+	UCHAR LatencyTimer = 1;
 	UCHAR FT_Byte;
 
 	UCHAR test[31] = {1, 2, 3, 0x4, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+	UCHAR testrx[31];
+
 	UCHAR * RxBuffer;
 	UCHAR * TxBuffer;
 
@@ -31,7 +33,7 @@ int main(int argc, char *argv[])
 	FILE * fp;
 	long  lsize;
 	size_t result;
-
+	int b;
 	// time related variables
 	clock_t start, stop;
 
@@ -61,52 +63,19 @@ int main(int argc, char *argv[])
 	// To configure FTDI in Synch mode		- FT_BITMODE_SYNC_FIFO
 	FT_SetConfiguration(Mask, FT_BITMODE_SYNC_FIFO, FT_ShowDebugInformation);
 
+/*
+	FT_SendBuffer(test, 8, 31);
+	FT_ReadBuffer(testrx, 8, 31);
 
-	//Open txt file and send content
-	fp = fopen("test_data.txt", "rb");
-	if (fp == NULL)
+	for(b = 0; b < 31; b = b + 1)
 	{
-		printf("test_data.txt file is not opened\n");
+		printf("Received byte 0x%x\n", testrx[b]);
 	}
+*/
 
-	// find out how long file size is
-	fseek(fp, 0, SEEK_END);
-	lsize = ftell(fp);
-	// go to file begginig othervise no data will be read out
-	rewind(fp);
-	printf("file size %d\n",lsize);
-
-	// dynamic memory allocation for receive and transmit buffers
-	TxBuffer = (char*) malloc (sizeof(char)*lsize);
-	RxBuffer = (char*) malloc (sizeof(char)*lsize);
-
-	if (TxBuffer == NULL)
-	{
-		printf("Could not allocate File_array");
-	}
-
-	result = fread(TxBuffer, 1, lsize, fp);
-
-	fclose(fp);
-
-	clock_t	begin = clock();
-	FT_SendBuffer(TxBuffer, lsize, lsize);
-	clock_t end = clock();
-
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("clocks per sec %i\n", CLOCKS_PER_SEC);
-	printf("time spent in ms:  %f\n", 1000*time_spent);
-
-	start = clock();
-	FT_ReadBuffer(RxBuffer, lsize, lsize);
-	stop = clock();
-
-	fp = fopen("received_data.txt", "w+");
-	fwrite(RxBuffer, 1, lsize, fp);
-	fclose(fp);
-
-//	free(File_array);
-
+//	FT_ProcessFile("lena512.bmp");
+//	FT_ProcessFile("test.txt");
+	FT_ProcessFile("test_img.JPG");
 	// Close FTDI port
 	FT_Close(ftHandle);
 }
